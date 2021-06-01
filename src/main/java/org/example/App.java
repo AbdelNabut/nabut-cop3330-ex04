0e97054beaa28cc1f8b7304f3a37c723ab5db46d
abdelnabut <AbdelNabut@outlook.com>
2021-05-31 21:48:33 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String noun, verb, adjective, adverb;
+        Scanner input = new Scanner(System.in);
+        System.out.print( "Enter a noun: ");
+        noun = input.next();
+        System.out.print( "Enter a verb: ");
+        verb = input.next();
+        System.out.print( "Enter a adjective: ");
+        adjective = input.next();
+        System.out.print( "Enter a adverb: ");
+        adverb = input.next();
+        System.out.printf("I like to %s with the %s %s %s", verb, adjective, noun, adverb);
+
+    }
+}
